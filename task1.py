3d9999681f3a3dbfdf7191ce5599e99e19e0e015
likithpoluparthi <likith.poluparthi@dal.ca>
2019-02-15 13:51:44 -0400

    task1.2

--- task1.py
+++ task1.py
@@ -2,7 +2,6 @@ f1= open('Book1.txt','r')
 f1=f1.readlines()
 def unique_words(f1):
   output=[]
-  lines=[]
   for line in f1:
     line=line.strip()
     line=line.split()
@@ -10,4 +9,14 @@ def unique_words(f1):
       if word not in output:
         output.append(word)
   print(output)
-unique_words(f1)
+def count_the_article(f1):
+  count=0
+  output=[]
+  for line in f1:
+    line=line.strip()
+    line=line.split()
+    for word in line:
+      count+=1
+  print(count)
+count_the_article(f1)
+  
